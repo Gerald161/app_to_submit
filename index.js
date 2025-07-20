11601ee1ec25a7b06be70632e7a18dc019b19371
Gerald161 <56877559+Gerald161@users.noreply.github.com>
2025-07-20 17:28:37 +0000

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const more_options_button = document.querySelector(".more_options_button");
-
-const small_navigator = document.querySelector(".small_navigator");
-
-more_options_button.addEventListener("click", (e)=>{
-    small_navigator.classList.toggle("reveal_small_navigator");
-})
